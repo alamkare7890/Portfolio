245dee19e7b8d59fb83ae9a3bf37b489323b2090
Masum Alam <masum.7788alam@gmail.com>
2022-07-08 15:45:33 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -3,14 +3,22 @@ var myToggle = document.getElementById('menuBth');
     menuBtn.onclick = function () {
 
     menuBtn.classList.toggle('active');
-
+    menuOpen.classList.toggle('slide');
 }
 
 
 $(document).ready(function () {
 
-   $()
+    $(window).scroll(function () {
+        $a = 50;
+            if(a  => 5) {
+                $('navFixed').addClass('nav-fixed');     
+            }else{
+                $('navFixed').removeClass('nav-fixed');     
+
+            }
     
+    }) 
 
-
+ 
 })
